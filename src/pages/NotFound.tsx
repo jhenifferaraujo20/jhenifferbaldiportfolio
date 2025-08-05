2b94e2d54b7770578762cc2c6fb4632afbe76404
jhenifferaraujo20 <jhenifferbaldi@gmail.com>
2025-08-05 12:29:45 -0300

    feat: cria estrutura inicial de rotas com páginas Index e NotFound

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+        <>
+            <h1>Erro 404</h1>
+            <p>Oops! Página não encontrada.</p>
+            <a href="/">Retornar para a home</a>
+        </>
+    );
+}
+
+export default NotFound;
